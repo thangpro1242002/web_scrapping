fa253020428ee74a9b35db3d19c49bd1f258ab44
thangpro1242002 <73933451+thangpro1242002@users.noreply.github.com>
2021-01-19 08:45:52 +0700

    Add files via upload

---
+++ CleaningYourDirtyData.py
@@ -0,0 +1,19 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+def getNgrams(content, n):
+  content = content.split(' ')
+  output = []
+  for i in range(len(content)-n+1):
+    output.append(content[i:i+n])
+  return output
+
+html = urlopen('http://en.wikipedia.org/wiki/Python_(programming_language)')
+bs = BeautifulSoup(html, 'html.parser')
+content = bs.find('div', {'id':'mw-content-text'}).get_text()
+ngrams = getNgrams(content, 2)
+print(ngrams)
+print('2-grams count is: '+str(len(ngrams)))
+
+
+
