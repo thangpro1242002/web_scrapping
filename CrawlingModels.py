d285a7670d163f21e61a507912e086212db7f483
thangpro1242002 <73933451+thangpro1242002@users.noreply.github.com>
2021-01-19 08:07:56 +0700

    Add files via upload

---
+++ CrawlingModels.py
@@ -0,0 +1,60 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def getPage(url):
+    """
+    Utilty function used to get a Beautiful Soup object from a given URL
+    """
+
+    session = requests.Session()
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'}
+    try:
+        req = session.get(url, headers=headers)
+    except requests.exceptions.RequestException:
+        return None
+    bs = BeautifulSoup(req.text, 'html.parser')
+    return bs
+#Xử lý các bố cục trang web khác nhau
+
+import requests
+
+class Content:
+    def __init__(self, url, title, body):
+        self.url = url
+        self.title = title
+        self.body = body
+
+
+def getPage(url):
+    req = requests.get(url)
+    return BeautifulSoup(req.text, 'html.parser')
+
+
+def scrapeNYTimes(url):
+    bs = getPage(url)
+    title = bs.find('h1').text
+    lines = bs.select('div.StoryBodyCompanionColumn div p')
+    body = '\n'.join([line.text for line in lines])
+    return Content(url, title, body)
+
+def scrapeBrookings(url):
+    bs = getPage(url)
+    title = bs.find('h1').text
+    body = bs.find('div', {'class', 'post-body'}).text
+    return Content(url, title, body)
+
+
+url = 'https://www.brookings.edu/blog/future-development/2018/01/26/delivering-inclusive-urban-access-3-uncomfortable-truths/'
+content = scrapeBrookings(url)
+print('Title: {}'.format(content.title))
+print('URL: {}\n'.format(content.url))
+print(content.body)
+
+url = 'https://www.nytimes.com/2018/01/25/opinion/sunday/silicon-valley-immortality.html'
+content = scrapeNYTimes(url)
+print('Title: {}'.format(content.title))
+print('URL: {}\n'.format(content.url))
+print(content.body)
+
