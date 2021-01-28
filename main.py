503724e322549f74dcbfa696187037ae7bbd2366
thangpro1242002 <73933451+thangpro1242002@users.noreply.github.com>
2021-01-28 15:05:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import luu_url
 def main():
     url_goc=input("Nhập link khởi đầu:")
     url_goc = input('Nhập link khởi đầu: ')
-    url_goc = quet_url.sua_url_goc(url_goc)
-    url_tim_duoc = quet_url.tim_url_lien_quan(url_goc, url_goc)
+    url_goc = web_url.sua_url_goc(url_goc)
+    url_tim_duoc = web_url.tim_url_lien_quan(url_goc, url_goc)
     waiting_line = url_tim_duoc
-    history = quet_url.them_va_duyet_hang_cho(waiting_line, url_goc)
+    history = web_url.them_va_duyet_hang_cho(waiting_line, url_goc)
     luu_url.tao_thu_muc('bong_da_2')
     luu_url.luu_tat_ca_file(history)
     
