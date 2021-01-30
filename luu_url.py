828d6aec953b6750d0ce438e188d2a55f4f19dfe
thangpro1242002 <73933451+thangpro1242002@users.noreply.github.com>
2021-01-30 16:29:42 +0700

    Update luu_url.py

--- luu_url.py
+++ luu_url.py
@@ -2,8 +2,6 @@
 # Các thư viện cần thiết
 import os
 import requests
-import codecs
-
 
 # Hàm Tạo thư mục và chuyển đến thư mục đó
 # Cần truyền vào cho hàm Tên thư mục
@@ -12,29 +10,16 @@ def tao_thu_muc(name):
     os.chdir(name)
 
 
-# Hàm Lưu file
-# Truyền vào url cần lưu và Số thứ tự để đặt tên file cần lưu
-def luu_file(url, stt):
-    file = codecs.open('file' + str(stt) + '.html', 'w', 'utf8')
-    file.write(requests.get(url).text)
-    file.close()
+#Hàm này để lưu lại các đường dẫn đã lấy vào thư mục vừa tạo
+def luu_file(data, name_folder):
+    #data là list chứa nội dung file html
+    path = "C:\Users\Admin\Desktop\web_scrapping"
+    os.chdir(path + str(name_folder))
 
 
-# Hàm lưu tất cả các url đã tim được
-# Truyền vào một List, set, tuples,... các URL hợp lệ
-def luu_tat_ca_file(history):
-    for (stt, url_con) in enumerate(history):
-        luu_file(url_con, stt)
-        print(f'{stt} {url_con}')
 
 
-def main():
-    tao_thu_muc('trioc')
-    luu_file('https://baomoi.com/', 2)
 
-
-if __name__ == "__main__":
-    main()
 
 
 
