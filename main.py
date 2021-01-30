4d1a25d3c97260c192e78290e343e4afb5ef7254
thangpro1242002 <73933451+thangpro1242002@users.noreply.github.com>
2021-01-30 17:06:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import web_url
 import luu_url
 
-def main():
-    url_goc=input("Nhập link khởi đầu:")
-    url_goc = input('Nhập link khởi đầu: ')
-    url_goc = web_url.sua_url_goc(url_goc)
-    url_tim_duoc = web_url.tim_url_lien_quan(url_goc, url_goc)
-    waiting_line = url_tim_duoc
-    history = web_url.them_va_duyet_hang_cho(waiting_line, url_goc)
-    luu_url.tao_thu_muc('bong_da_2')
-    luu_url.luu_tat_ca_file(history)
-    
-
-    # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    start()
+def start():
+    url_list = ['https://vietnamnet.vn/']
+    history = []  # DS các đường dẫn đã duyệt
+    max_page = 15  # quy định số lượng trang web được tải về
+    count = 0  # đếm số lượng trang web đã tải về
+    data_folder = "C:\Users\Admin\Desktop\web_scrapping"
+    # Tạo Folder
+    folder.kiem_tra_folder(data_folder)
+    # kịch bản tải các trang web
+    while (count < max_page) and (len(url_list) > 0):
+        url = url_list.pop(0)
+        # Đọc nội dung
+        page = web_url.doc_noi_dung(url)
+        # Lấy tất cả đường dẫn
+        links = web_url.lay_duong_link((page))
+        for item in links:
+            if not web_url.kiem_tra_link(item):
+                item = web_url.chinh_sua_link("https://vietnamnet.vn/", item)
+            if not ((item in url_list) and (item in history)):
+                url_list.append(item)
+        luu_url.luu_noi_dung(page, data_folder, count)
+        history.append(url)
+        # đếm số đường dẫn đã duyệt
+        count += 1
