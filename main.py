1437791d6901f68fc9c41a4c3e27378d38a59cd7
thangpro1242002 <73933451+thangpro1242002@users.noreply.github.com>
2021-01-31 09:29:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ def start():
         item = web1.lay_duong_dan(content)
         for i in item:
         if web1.kiem_tra_duong_dan(i) == False:   ## Kiểm tra đường dẫn
-            i = web1.chinh_sua_duong_dan(url_first, i)       
+            i = web_url.chinh_sua_duong_dan(url_first, i)       
         if (i not in history) and (i not in url_list):
-            if web1.kiem_tra_url_da_duyet(i) == True:
+            if web_url.kiem_tra_url_da_duyet(i) == True:
                 continue    
             else:    
                 url_new.append(i)       ###Thêm đường dẫn hợp lệ vào url_new
