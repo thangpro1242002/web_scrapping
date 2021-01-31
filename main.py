08d0c2c9f128becbf968fa7e119be013a05b83d0
thangpro1242002 <73933451+thangpro1242002@users.noreply.github.com>
2021-01-31 09:26:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,27 +2,30 @@ import web_url
 import luu_url
 
 def start():
-    url_list = ['https://vietnamnet.vn/']
-    history = []  # DS các đường dẫn đã duyệt
-    max_page = 15  # quy định số lượng trang web được tải về
+    home_page = input("nhap link :")  #trang chủ để chứa các đường link
+    url_list = history = []  # DS các đường dẫn đã duyệt
+    max_page =int(input("nhập số trang bạn muốn tải:"))  #Nhập số lượng trang web mà bạn muốn tải
     count = 0  # đếm số lượng trang web đã tải về
     data_folder = "C:\\webscrapping\\"
     # Tạo Folder
     folder.tao_thu_muc(data_folder)
+    url_list.append(home_page) #thêm đường link mình nhập từ domain vào list url_list
     # kịch bản tải các trang web
-    while (count < max_page) and (len(url_list) > 0):
-        url = url_list.pop(0)
+    while (count < max_page) and (len(url_list) > 0):   #sử dụng vòng lặp while với điều kiện biến đếm phải nhỏ hơn số lượng của page và độ dài của link phải lớn hơn 0
+        url = url_list.pop(0)  # Lấy phần tử đầu tiên của url_list
+        history.append(url)  # thêm phần tử vào history
         # Đọc nội dung
-        page = web_url.doc_noi_dung(url)
+        data = web_url.doc_noi_dung(url) 
         # Lấy tất cả đường dẫn
-        links = web_url.lay_duong_dan((page))
-        for item in links:
-            if not web_url.kiem_tra_duong_dan(item):
-                item = web_url.chinh_sua_duong_dan("https://vietnamnet.vn/", item)
-            if not ((item in url_list) and (item in history)):
-                url_list.append(item)
-        luu_url.luu_noi_dung(page, data_folder, count)
-        history.append(url)
+        item = web1.lay_duong_dan(content)
+        for i in item:
+        if web1.kiem_tra_duong_dan(i) == False:   ## Kiểm tra đường dẫn
+            i = web1.chinh_sua_duong_dan(url_first, i)       
+        if (i not in history) and (i not in url_list):
+            if web1.kiem_tra_url_da_duyet(i) == True:
+                continue    
+            else:    
+                url_new.append(i)       ###Thêm đường dẫn hợp lệ vào url_new
         # đếm số đường dẫn đã duyệt
         count += 1
       
